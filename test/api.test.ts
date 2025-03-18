ba5c17a1b0ea0e69619d28992fd9f6bfe45d3c75
Brenwell <802618+brenwell@users.noreply.github.com>
2025-03-18 15:47:45 +0100

    fix tests again

--- test/api.test.ts
+++ test/api.test.ts
@@ -7,19 +7,41 @@ const basicAuth = Buffer.from(`${basicAuthUser}:${basicAuthPass}`).toString(
   "base64"
 );
 
-console.log(`Authorization:Basic ${basicAuth}`);
+describe("POST /thread/:threadId", () => {
+  it("should not be authorized", async () => {
+    await request(url)
+      .post("/thread/unauthorized-user-1-a")
+      .set("Authorization", `Basic bullshit`)
+      .send({ message: "Hello, this is a test message" })
+      .expect("Content-Type", /text/)
+      .expect(401);
+  });
+
+  it("should post to thread", async () => {
+    const response = await request(url)
+      .post("/thread/authorized-user-1-a")
+      .set("Authorization", `Basic ${basicAuth}`)
+      .send({ message: "Hello, this is a test message" })
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    // Add assertions for the response body here
+    expect(response.body).toHaveProperty("response");
+  }, 10000);
+});
 
 describe("GET /thread/:threadId", () => {
   it("should not be authorized", async () => {
     await request(url)
-      .get("/thread/some-loser-1")
+      .get("/thread/unauthorized-user-1-a")
+      .set("Authorization", `Basic bullshit`)
       .expect("Content-Type", /text/)
       .expect(401);
   });
 
   it("should not return thread data", async () => {
     const response = await request(url)
-      .get("/thread/some-loser-1")
+      .get("/thread/nonexisitent-user-1-a")
       .set("Authorization", `Basic ${basicAuth}`)
       .expect("Content-Type", /json/)
       .expect(404);
@@ -30,7 +52,7 @@ describe("GET /thread/:threadId", () => {
 
   it("should return thread data", async () => {
     const response = await request(url)
-      .get("/thread/test-user-1-1")
+      .get("/thread/authorized-user-1-a")
       .set("Authorization", `Basic ${basicAuth}`)
       .expect("Content-Type", /json/)
       .expect(200);
@@ -39,27 +61,4 @@ describe("GET /thread/:threadId", () => {
     expect(response.body).toHaveProperty("messages");
     expect(response.body.messages.length).toBeGreaterThan(0);
   });
-});
-
-describe("POST /thread/:threadId", () => {
-  it("should not be authorized", async () => {
-    await request(url)
-      .post("/thread/some-loser-1")
-      .set("Authorization", `Basic ${basicAuth}`)
-      .send({ message: "Hello, this is a test message" })
-      .expect("Content-Type", /text/)
-      .expect(401);
-  });
-
-  it("should post to thread", async () => {
-    const response = await request(url)
-      .post("/thread/test-user-1-1")
-      .set("Authorization", `Basic ${basicAuth}`)
-      .send({ message: "Hello, this is a test message" })
-      .expect("Content-Type", /json/)
-      .expect(200);
-
-    // Add assertions for the response body here
-    expect(response.body).toHaveProperty("response");
-  }, 10000);
 });
